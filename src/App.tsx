796513cd1390b24fb65f9438b41806f7a8803549
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-29 23:59:04 +0300

    layout added

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header_logo'>
+
+          </div>
+        </div>
       </header>
     </div>
   );
